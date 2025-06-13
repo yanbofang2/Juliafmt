bf0c2ffafd9492d09963252042b60eb2e958e624
Documenter.jl <documenter@juliadocs.github.io>
2025-06-13 21:09:20 +0000

    build based on 41ad76c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Juliafmt","page":"Home","title":"Juliafmt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Juliafmt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
